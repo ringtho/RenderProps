a286407c6c48ad7234390bb564645db9b2ff5e4c
Smith Ringtho <sringtho@gmail.com>
2023-01-12 20:09:40 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import DataFetcher from "./DataFetcher"
+
+function App() {    
+    return (
+        <div>
+            <DataFetcher url="https://swapi.co/api/people/1">
+                {({data, loading}) => (
+                    loading ? 
+                    <h1>Loading...</h1> :
+                    <p>{JSON.stringify(data)}</p>
+                )}
+            </DataFetcher>
+        </div>
+    )
+}
+
+export default App
